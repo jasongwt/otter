aadba85bb05b3d2f38e62c2e4123988e8e019014
sunpengfei <sunpengfei@windimg.com>
2019-05-22 20:02:29 +0800

    add prometheus

--- manager/deployer/src/main/java/com/alibaba/otter/manager/deployer/JettyEmbedServer.java
+++ manager/deployer/src/main/java/com/alibaba/otter/manager/deployer/JettyEmbedServer.java
@@ -16,7 +16,6 @@
 
 package com.alibaba.otter.manager.deployer;
 
-import com.alibaba.otter.canal.spi.CanalMetricsProvider;
 import com.alibaba.otter.canal.spi.CanalMetricsService;
 import com.alibaba.otter.canal.spi.NopCanalMetricsService;
 import org.eclipse.jetty.server.Handler;
@@ -26,10 +25,6 @@ import org.eclipse.jetty.webapp.WebAppContext;
 import org.eclipse.jetty.xml.XmlConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ServiceLoader;
 
 /**
  * 嵌入式jetty启动
@@ -43,8 +38,6 @@ public class JettyEmbedServer {
     private static final String DEFAULT_CONFIG = "jetty.xml";
     private Server              server;
     private String              config         = DEFAULT_CONFIG;
-    private CanalMetricsService metrics = NopCanalMetricsService.NOP;
-    private int metricsPort;
 
     public JettyEmbedServer(String jettyXml){
         this.config = jettyXml;
@@ -71,34 +64,8 @@ public class JettyEmbedServer {
         if (logger.isInfoEnabled()) {
             logger.info("##Jetty Embed Server is startup!");
         }
-        loadCanalMetrics();
-        if (metricsPort <= 0) {
-            metricsPort = 11316;
-        }
-        metrics.setServerPort(metricsPort);
-        metrics.initialize();
     }
 
-    private void loadCanalMetrics() {
-        ServiceLoader<CanalMetricsProvider> providers = ServiceLoader.load(CanalMetricsProvider.class);
-        List<CanalMetricsProvider> list = new ArrayList<CanalMetricsProvider>();
-        for (CanalMetricsProvider provider : providers) {
-            list.add(provider);
-        }
-        if (!list.isEmpty()) {
-            // 发现provider, 进行初始化
-            if (list.size() > 1) {
-                logger.warn("Found more than one CanalMetricsProvider, use the first one.");
-                // 报告冲突
-                for (CanalMetricsProvider p : list) {
-                    logger.warn("Found CanalMetricsProvider: {}.", p.getClass().getName());
-                }
-            }
-            // 默认使用第一个
-            CanalMetricsProvider provider = list.get(0);
-            this.metrics = provider.getService();
-        }
-    }
 
     public void join() throws Exception {
         server.join();
@@ -112,7 +79,6 @@ public class JettyEmbedServer {
         if (logger.isInfoEnabled()) {
             logger.info("##Jetty Embed Server is stop!");
         }
-        metrics.terminate();
     }
 
     // ================ setter / getter ================
--- node/etl/src/main/java/com/alibaba/otter/node/etl/select/selector/canal/CanalEmbedSelector.java
+++ node/etl/src/main/java/com/alibaba/otter/node/etl/select/selector/canal/CanalEmbedSelector.java
@@ -207,6 +207,7 @@ public class CanalEmbedSelector implements OtterSelector {
                 return instance;
             }
         });
+        canalServer.setMetricsPort(11316);
         canalServer.start();
 
         canalServer.start(destination);
